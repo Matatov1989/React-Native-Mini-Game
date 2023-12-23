764b3dac7a829838325f34065bc507915b48ba46
Yury <matatov1989@gmail.com>
2023-12-23 12:36:24 +0200

    styling text input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,7 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput style={styles.numberInput} maxLength={2} />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -21,9 +21,20 @@ const styles = StyleSheet.create({
     backgroundColor: "#72063c",
     borderRadius: 8,
     elevation: 4,
-    shadowColor: 'black',
-    shadowOffset: {width: 0, height: 2},
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
-    shadowOpacity: 0.25
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center'
   },
 });

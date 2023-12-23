7095fe2969f9c0beb61c7b6dddcb0d35c0321285
Yury <matatov1989@gmail.com>
2023-12-23 17:42:49 +0200

    switching screens programmatically

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
-function StartGameScreen() {
+function StartGameScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState("");
 
   function numberInputHandler(enteredText) {
@@ -20,6 +20,8 @@ function StartGameScreen() {
       );
       return;
     }
+
+    onPickNumber();
   }
 
   function resetInputHandler() {
